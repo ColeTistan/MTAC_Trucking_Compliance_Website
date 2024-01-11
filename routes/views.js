8d3ac5f84f95d97a23effdf855d2ca13d19bc295
coletistan <coletistan@gmail.com>
2024-01-11 11:42:23 -0500

    Added article template and refactored footer styling

--- routes/views.js
+++ routes/views.js
@@ -4,6 +4,8 @@ const viewsRouter = express.Router();
 global.isHomePage = true;
 
 viewsRouter.get("/", (req, res) => {
+  const token = req.cookies.token;
+  console.log(token);
   return res.render("index");
 });
 
@@ -14,5 +16,9 @@ viewsRouter.get("/login", (req, res) => {
 viewsRouter.get("/signup", (req, res) => {
   res.render("signup");
 });
+
+viewsRouter.get("/news", (req, res) => {
+  res.render("article");
+})
 
 module.exports = viewsRouter;
