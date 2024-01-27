38f2442ace5354a1fef661021a31592d778fbeb5
coletistan <coletistan@gmail.com>
2024-01-26 22:21:25 -0500

    Refactored view routes to render separate page components.

--- routes/views.js
+++ routes/views.js
@@ -1,26 +1,40 @@
 const express = require("express");
 const viewsRouter = express.Router();
 
+const getToken = (req) => {
+  return req.cookies.token;
+}
+
 viewsRouter.get("/", (req, res) => {
-  let token = req.cookies.token;
-  console.log(token);
-  return res.render("index", { token });
+  return res.render("index", { token: getToken(req) });
+});
+
+viewsRouter.get("/about", (req, res) => {
+  res.render("about", { token: getToken(req) });
+});
+
+viewsRouter.get("/service", (req, res) => {
+  res.render("service", { token: getToken(req)});
+});
+
+viewsRouter.get("/links", (req, res) => {
+  res.render("links", { token: getToken(req)});
+});
+
+viewsRouter.get("/news", (req, res) => {
+  res.render("news", { token: getToken(req)});
+});
+
+viewsRouter.get("/contact", (req, res) => {
+  res.render("contact", { token: getToken(req)});
+});
+
+viewsRouter.get("/article", (req, res) => {
+  res.render("article", { token: getToken(req)});
 });
 
 viewsRouter.get("/login", (req, res) => {
   res.render("login");
-});
-
-viewsRouter.get("/signup", (req, res) => {
-  res.render("signup");
-});
-
-viewsRouter.get("/article", (req, res) => {
-  res.render("article");
-});
-
-viewsRouter.get("/news", (req, res) => {
-  res.render("news");
 });
 
 module.exports = viewsRouter;
