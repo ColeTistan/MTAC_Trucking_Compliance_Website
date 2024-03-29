ca346ec34aaf005d3d2b324f43575027150df5d9
coletistan <coletistan@gmail.com>
2024-03-29 10:12:42 -0400

    Added new testimonial and updated connection logging.

--- connect.js
+++ connect.js
@@ -5,12 +5,12 @@ mongoose.set("strictQuery", false);
 dotenv.config();
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI)
-        console.log("connected successfully...");
-    } catch (err) {
-        console.error(`failed to connect to server: ${err}`);
-    }
-}
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`database connected successfully: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(`failed to connect to server: ${err}`);
+  }
+};
 
 module.exports = connectDB;
