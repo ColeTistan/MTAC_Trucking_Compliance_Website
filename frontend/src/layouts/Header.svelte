798b90fca39e946178c225d71aa9eea63bc5b98c
coletistan <coletistan@gmail.com>
2024-04-22 13:13:32 -0400

    Added navbar layout to svelte application.

---
+++ frontend/src/layouts/Header.svelte
@@ -0,0 +1,27 @@
+<script>
+</script>
+
+<header>
+  <nav>
+    <h1>M-Tac Trucking Compliance LLC</h1>
+    <ul>
+      <li><a href="/">About</a></li>
+      <li><a href="/">Insight</a></li>
+      <li><a href="/">Contact</a></li>
+    </ul>
+  </nav>
+</header>
+
+<style>
+  h1 {
+    @apply text-xl tracking-wide;
+  }
+
+  header {
+    @apply shadow-lg shadow-gray-200;
+  }
+
+  nav {
+    @apply flex justify-between items-center px-28 py-4 bg-amber-400;
+  }
+</style>
