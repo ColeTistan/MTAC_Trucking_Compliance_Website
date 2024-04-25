f002622d0843bb57a7f301b3994f0d825294135b
coletistan <coletistan@gmail.com>
2024-04-25 15:24:39 -0400

    added navbar using svelte and tailwind css

---
+++ frontend/src/assets/Button.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let buttonName, externalLink;
+</script>
+
+<a class="button" href={externalLink}>{buttonName}</a>
+
+<style>
+  .button {
+    @apply transition-all duration-300 bg-amber-400 text-gray-800 rounded-full px-7 hover:bg-amber-500;
+  }
+</style>
--- frontend/src/layouts/Header.svelte
+++ frontend/src/layouts/Header.svelte
@@ -1,27 +1,35 @@
 <script>
+  import Button from "../assets/Button.svelte";
+  import logo from "../../../public/img/company_logo.png"
 </script>
 
 <header>
   <nav>
-    <h1>M-Tac Trucking Compliance LLC</h1>
+    <a class="logo" href="/">
+      <img src="{logo}" alt="logo" />
+    </a>
     <ul>
       <li><a href="/">About</a></li>
       <li><a href="/">Insight</a></li>
       <li><a href="/">Contact</a></li>
+      <li><Button externalLink={"/"} buttonName={"Login"} /></li>
     </ul>
   </nav>
 </header>
 
 <style>
-  h1 {
-    @apply text-xl tracking-wide;
+  .logo {
+    @apply w-36 bg-transparent p-0;
   }
+  /* h1 {
+    @apply text-xl font-medium tracking-wide;
+  } */
 
   header {
     @apply shadow-lg shadow-gray-200;
   }
 
   nav {
-    @apply flex justify-between items-center px-28 py-4 bg-amber-400;
+    @apply flex justify-between items-center px-28 py-4 bg-gray-800;
   }
 </style>
